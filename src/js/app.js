1666a28dedfec7357dfd5c3f5e189c284ada5674
bensongardner <bensongardner@yahoo.com>
2018-03-30 16:17:12 -0500

    fix: combine 2 js files into 1

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,44 @@
+// This file was previously broken into data.js and viewModel.js, which could be donee again if we can solve the asynch issue with callbacks/promises.
+
+// markerData (location, title, possibly type of place [filterable], possibly the characteristic of whether or not there is a related photo, and any photo from a nearby spot pulled from the third-party apis)
+
+// I think the initial lat/lng, zoom, etc values required to load the map... .? Does that make sense? Or becuase it has to be loaded reight away, do we not want to bother having this in the data model? 
+
+// Map data??? Or does this all stay in google maps
+
+// any data required re: Flickr/etc? I don't think so.
+
+// if the input field is more than just a search of the titles, then there will be some data options assoc.
+
+// Which marker is highlighted(if any)
+//THE RELATIONSHIP OF ABOVE LINE AND BELOW LINE IS INTERESTING....
+// the current value of the input (search filter) field (i.e., what markers/list items would be currently displayed)
+
+// 
+
+
+var data = {
+    mapStart: {lat: 34.0488884, lng: -118.2404842},
+    
+    placeData: [
+        {
+            title: 'Japanese Village Plaza', 
+            position: {lat: 34.0488884, lng: -118.2404842}
+        },
+        {
+            title: 'Mitsuru Cafe',
+            position: {lat: 34.0489316, lng: -118.2416973}
+        }
+    ],
+    
+    filter: '',
+    
+    
+
+    
+};
+
+
 var viewModel = {
     
     placesArray: ko.observableArray(data.placeData),
@@ -162,10 +203,8 @@ var viewModel = {
     markers: [],
 
     initMap: function() {
-
-        console.log(this.map);
         
-        viewModel.map = new google.maps.Map(document.getElementById('map'), {
+        this.map = new google.maps.Map(document.getElementById('map'), {
             center: data.mapStart, 
             zoom: 13,
             styles: this.mapStyles,
@@ -173,14 +212,14 @@ var viewModel = {
         });
       
         // Create markers appearing on initialize
-        for (i = 0; i < placeData.length; i++) {
+        for (i = 0; i < data.placeData.length; i++) {
             var marker = new google.maps.Marker({
                 position: data.placeData[i].position,
                 map: this.map,
                 styles: viewModel.mapStyles,
                 title: data.placeData[i].title
             });
-        markers.push(marker);
+        this.markers.push(marker);
         };
         
         // Add an infoWindow to each marker either inside the above loop or in a separate loop.
@@ -207,9 +246,10 @@ var viewModel = {
 
 };
 
+/*
 Promise.all([data, knockout]).then(function() {
     ko.applyBindings(viewModel);
 }, function() {
     document.getElementById('list').append('Failed to load required files. Please try refreshing the page.');
 });
-
+*/
