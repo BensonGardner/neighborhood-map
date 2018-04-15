62d0cd744cc547206862d1f14b30b3e1f4bd5d38
bensongardner <bensongardner@yahoo.com>
2018-04-14 19:09:22 -0500

    feat: Implement functioning list filter

--- src/js/app.js
+++ src/js/app.js
@@ -8,17 +8,20 @@ var data = {
         {
             title: 'Japanese Village Plaza', 
             position: {lat: 34.0488884, lng: -118.2404842},
-            feature: 'Anime Idol Competition'
+            feature: 'Anime Idol Competition',
+            selected: false
         },
         {
             title: 'Mitsuru Cafe',
             position: {lat: 34.0489316, lng: -118.2416973},
-            feature: 'Delicious sweet/salty dango'
+            feature: 'Delicious sweet/salty dango',
+            selected: false
         },
         {
             title: 'test',
             position: {lat: 34.5, lng: -118.5},
-            feature: ''
+            feature: '',
+            selected: false
         }
     ],
     
@@ -29,8 +32,6 @@ var viewModel = function() {
     var self = this;
     
     this.selectPlace = function() {
-        console.log('selected! ' + this.title);
-        
         // Needs to be called by 
         // list item 
         
@@ -48,160 +49,42 @@ var viewModel = function() {
     
     // Make a copy of the placeData array so we can alter
     // it as needed.
-    // It looks like there are some array methods that are
-    // more elegant that might be better here.
-    this.listArray = ko.observableArray(JSON.parse(JSON.stringify(data.placeData)));
-
-    // Return a version of listArray that only includes
-    // entries which contain the filter.
-    // (It works like this: the .indexOf() method will
-    // return a value of -1 if the term is not present.  
-    // If the search term IS present, then 0 is the lowest 
-    // possible value. So adding one and testing for 
-    // truthiness should result in a test for whether or 
-    // not the term is present.)
-    this.filteredIn = ko.pureComputed(function(){
-        console.log(this);
-        return (this.listArray().title.indexOf(filter) + 1);
-    }, this);
-    
-    /* COMMENTING THIS OUT FOR NOW --
-       I am trying to use the filteredList 
-       approach instead. When I tried this, 
-       I got the message that "display"
-       was not defined when I referenced it in the data
-       binding in index.html.
-       
-    // The computed function in this loop
-    // gets attached to the placeData array to 
-    // keep track of whether one of the list items' 
-    // title or feature properties contains the 
-    // text entered in the filter. 
-    for (i = 0; i < this.listArray.length; i++) {
-
-        var text = this.listArray[i].title + ' ' + 
-            this.listArray[i].feature;
-        
-        this.listArray[i].display = ko.computed(function() {
-            if (filter() == '') {
-                return 'block';
-            }
-            if (text.indexOf(filter()) == -1) {
-                return 'none';
-            } else {
-                return 'block';
-            };
-        });
-        
-        console.log('display() for ' + this.listArray[i].title + ' is ' + this.listArray[i].display());
+    // In future, this may be updated with a 
+    // newer ECMA option.
+    this.listArray = ko.observableArray(JSON.parse(JSON.stringify(data.placeData))); 
     
     this.listArray().forEach(function(item) {
-        item.selected = ko.observable('notSelected');
+
+        // Determine whether each item should be filtered in
+        // (i.e. displayed in the list) by using a ko.computed 
+        // observable.
+        item.filteredIn = ko.computed(function(){
+            // Break the text input into an array of separate 
+            // words, case insensitive.
+            filterWords = filter().toLowerCase().split(' ');
+            
+            // Put all the item together in case insensitive fashion.
+            itemInfo = (item.title + ' ' + item.feature).toLowerCase();
+            
+            // Use indexOf() to search itemInfo for each word
+            // from the text input. indexOf() will return a 
+            // value of 0 or more indicating the spot where the 
+            // search term appears, or -1 if it doesn't appear.
+            
+            // We'll add 1 so that the non-appearing words will be 
+            // falsey and all appearing words will be truthy. 
+            // We'll multiply these values by each other for each 
+            // word in the input box, since we want every term inputted
+            // to appear. 
+            
+            var value = 1;
+            for (i = 0; i < filterWords.length; i++) {
+                value *= (itemInfo.indexOf(filterWords[i]) + 1);
+            };
+            return value;
+        });
     });
-    
-    /*this.listArray().forEach(function(item) {
-        console.log(item.selected());
-    });*/
-    
-    // This line creates a copy of the placeData array 
-    // from our data model. We need to use the JSON methods
-    // in order to grab the exact same content but ensuring
-    // it's a copy, instead of the exact same object. 
-    // If it were the latter, our Knockout methods would
-    // alter our original data. 
-    
-/*    this.filteredList = ko.computed(function() {
-    
-        if (this.filter === '') {
-            return this.listArray();
-        } else {       
-            this.listArray().filter(function() {
-                this.listArray.remove(function (item) { 
-                    return ((item.title + item.feature).indexOf(this.filter) != -1); 
-                }) 
-            })
-        };  
-    });*/
-                                    
-    // NOTE TO SELF: I think we need to try to computed() 
-    // idea after all. The filter thing isn't working
-    // because the length of the array (and thus the index number
-    // of each entry) changes the instant something is
-    // filtered out. Maybe we could also use the id instead
-    // of the array index?? 
-    
-    // What was the problem with teh computable again?
-    // maybe it was something else i hadn't figure out
-    // yet at that point.
-    
-    // Subscribe to the filter. Wheneger it 
-    // changes, do two things: update the markers
-    // and update the listArray
 
-   /* this.filter.subscribe(function(newValue) {
-        
-        var self = this;
-         
-        console.log(self.markers);
-        // renew both the listArray and 
-        // markers to their initial state 
-        // (i.e., including all places from 
-        // placeData)
-        console.log(this.listArray().length + '. ');
-        
-        //placesArray = JSON.parse(JSON.stringify(data.placeData));
-        //this.listArray(JSON.parse(JSON.stringify(data.placeData)));
-        
-        console.log(this.listArray().length + '. ' + data.placeData.length);
-        
-        markers.length = 0;
-        createMarkers();
-        console.log(filter());
-        
-        // The following code does not seem to 
-        // be the right solution. It is removing
-        // the wrong items, not enough items, etc.
-        // I'm guessing this is because the position
-        // of each item in the array is changing 
-        // the instant the first item is removed...
-        // therefore, I'm looking to use a computed()
-        // value for this purpose instead of iterating through
-        // the listArray.
-        
-        // Next, remove all places that don't have
-        // newValue in their title or feature property
-        // Need to figure out case issue.
-        // Right now, doesn't always filter correctly
-        // might be due to length of array changing mid-
-        // process??
-        
-       /*for (i = 0; i < placesArray.length; i++) {
-            var currentPlaceItem = this.listArray()[i],
-                currentMarker = markers[i],
-                currentPlace = data.placeData[i];
-            console.log((currentPlace.title + currentPlace.feature).indexOf(filter()));
-            if ((currentPlace.title + currentPlace.feature).indexOf(filter()) == -1) {
-                console.log((currentPlace.title + currentPlace.feature).indexOf(filter()));
-                console.log("removing " + currentPlaceItem.title);
-                this.listArray.remove(currentPlaceItem);
-                console.log(data.placeData);
-            };
-            // DO SOMETHING TO HIDE SELECTED MARKER
-        };
-
-    });   */
-
-
-      /* this is now above  
-        this.filterTrue = ko.computed(function() {
-        
-            if ((this.title + this.feature).indexOf(filter()) != -1) {
-                return 'block'
-            } else {
-                return 'none'
-            };
-        }); 
-    */
 };
 
 var mapControl = {
@@ -211,8 +94,6 @@ var mapControl = {
     markers: [],
 
     initMap: function() {
-        
-        console.log(this + " is this");
 
         this.map = new google.maps.Map(document.getElementById('map'), {
                 center: data.mapStart, 
@@ -227,155 +108,62 @@ var mapControl = {
 
     },
 
-    // this.selectPlace is called by both 
-    // list items and markers.
-
-    // This function takes the current (selected)
-    // object in the listArray, highilghts that 
-    // item, calls the method to select and animate
-    // the marker It selects a current list item 
-    // and a current marker, highlighting the 
-    // latter and animating the former, and opening
-    // an infowindow on the map. 
     selectPlace: function(place) {
         // First remove any other selection
         // that might be in play.
-        console.log(this);
-        listArray().forEach(function(item) {
-            console.log(listArray());
-            console.log(item.title);
-            console.log(item.selected);
-            item.selected('notSelected');
-            console.log(item.selected);
-        });
-        console.log('selected! ' + place.title);
-
-        var selectedMarker, 
-            selectedListItem;
         
-        // Next iterate through listArray
-        // and the markers array to find the 
-        // matching item.
-        for (i = 0; i < listArray().length; i++) {
-            console.log(listArray()[i].title);
-            if (listArray()[i].title = place.title) {
-                var selectedListItem = listArray()[i];
-                console.log('selected! ' + selectedListItem.title);
-            };
-        if (!selectedListItem) {
-                throw 'Invalid place selected';
-            } 
-        };
+        // Then update which place is selected and re-render markers
         
-        for (i = 0; i < markers.length; i++) {
-            if (markers[i].title = place.title) {
-                var selectedMarker = markers[i];
-            };
-            
-        if (!selectedMarker) {
-                throw 'Invalid place selected';
-            };   
-        };
-        
-        console.log(selectedListItem.selected);
-        console.log(selectedMarker.title);
-        
-        selectedListItem.selected('selected');
-        
-        // This call is not working. Should I be doing
-        // detailinfowindow.open?
-        // openWindow(selectedMarker, detailInfowindow);   
-        detailInfowindow.open(selectedMarker); 
-    },
-           
-    updateMarkers: function() {
-        for (i = 0; i < this.markers; i++) {
-            markers.forEach(function() {
-                this.visibility = "off";
-                if (this.filter) {
-                    this.visibility = "on";
-                };
-            })
-        }
-    },
     
+        mapControl.createMarkers();
+
+        // Then open the right infowindow
+        
+        mapControl.detailInfowindow.open(map, selectedMarker); 
+    
+    },
+        
     createMarkers: function() {
-        for (i = 0; i < data.placeData.length; i++) {
-            var marker = new google.maps.Marker({
-                position: data.placeData[i].position,
-                map: this.map,
-                styles: mapStyles,
-                title: data.placeData[i].title,
-                filter: data.placeData[i].filter
-            });
-            this.markers.push(marker);
-        };
-    },
-    
-    detailInfowindow: function() {
-        var newInfoWindow = new google.maps.InfoWindow({
-            content: 'test' // this.title
-        })
-    },
-    
-    openWindow: function(marker, infowindow) {
-        console.log('infowindow opened');
-        // Check to make sure the infowindow is not already opened on this marker.
-        if (infowindow.marker != marker) {
-            infowindow.marker = marker;
-            infowindow.setContent('<div>' + marker.title + '</div>');
-
-            // Add Flickr API info here.
-
-            infowindow.open(map, marker);
-            // Make sure the marker property is cleared if the infowindow is closed.
-            infowindow.addListener('closeclick', function() {
-                infowindow.marker = null;
-            });
-        }
-    },
-    
-    // This function, patterned on something 
-    // very similar in Udacity's coursework
-    // leading up to this project, takes a color
-    // and makes a marker in that hue.
-    makeMarkerIcon: function(markerColor) {
         var markerImage = new google.maps.MarkerImage(
-          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
-          '|40|_|%E2%80%A2',
+          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|2B6D06|40|_|%E2%80%A2',
             new google.maps.Size(21, 34),
             new google.maps.Point(0, 0),
             new google.maps.Point(10, 34),
             new google.maps.Size(21,34));
-        return markerImage;
-    },
-
-    placeIcon: function() {
-        makeMarkerIcon('2B6D06')
-    },
-    
-    createMarkers: function() {
         for (i = 0; i < data.placeData.length; i++) {
+            var visibility = Boolean(data.placeData[i].title.indexOf(filter) + 1);
             var marker = new google.maps.Marker({
                 position: data.placeData[i].position,
                 map: this.map,
-                styles: mapStyles,
                 title: data.placeData[i].title,
+                setVisible: visibility,
                 feature: data.placeData[i].feature,
-                //icon: this.placeIcon
+                icon: markerImage
             });
             this.markers.push(marker);
+            console.log(marker.title);
+            var markerTitle = marker.title;
             marker.addListener('click', function() {
                 // For some reason, "this.title" is locked
                 // to be whatever the very first click was on
-                console.log('this.title is ' + this.title + '. And marker.title is ' + marker.title);
+                console.log('this.parent is ' + this.parentNode + '. And marker.title is ' + markerTitle);
                 // For some reason if you keep clicking around,
                 // this.title eventually becomes undefined. 
-                selectPlace(this)
+                // Also, when you click on a marker "this" is 
+                // undefined.
+                mapControl.selectPlace(this);
+                mapControl.detailInfoWindow.open(map, this);  
             });
         };
     },
     
+    detailInfowindow: function(selectedMarker) {
+        var newInfoWindow = new google.maps.InfoWindow({
+            content: selectedMarker.title
+            // Flickr content will be pulled from data obj here
+        })
+    },
+  
     // I might not need the below function depending
     // on how other stuff shakes down
     updatePlaces: function() {
@@ -385,15 +173,6 @@ var mapControl = {
         // which should be recorded in data
     }
 };
-    
-/* Need to figure out why this wasn't working:
-
-Promise.all([data, knockout]).then(function() {
-    ko.applyBindings(viewModel);
-}, function() {
-    document.getElementById('list').append('Failed to load required files. Please try refreshing the page.');
-});
-*/
 
 var init = function() {
     ko.applyBindings(viewModel());
