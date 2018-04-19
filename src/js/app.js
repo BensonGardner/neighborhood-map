235249101e3ebcb822692a1094d0c73c2b160ecb
bensongardner <bensongardner@yahoo.com>
2018-04-18 22:22:05 -0500

    feat: Implement map filter.

--- src/js/app.js
+++ src/js/app.js
@@ -31,10 +31,12 @@ var viewModel = function() {
     
     var self = this;
     
+    this.filterWords = [];
+    
     this.selectPlace = function() {
         // Needs to be called by 
         // list item 
-        
+            
         // Select that marker and 
         // that list item (through a similar id?),
         // toggle the selected class on the li,
@@ -61,30 +63,40 @@ var viewModel = function() {
         item.filteredIn = ko.computed(function(){
             // Break the text input into an array of separate 
             // words, case insensitive.
-            filterWords = filter().toLowerCase().split(' ');
+            self.filterWords = filter().toLowerCase().split(' ');
             
             // Put all the item together in case insensitive fashion.
-            itemInfo = (item.title + ' ' + item.feature).toLowerCase();
+            var itemInfo = (item.title + ' ' + item.feature).toLowerCase();
             
             // Use indexOf() to search itemInfo for each word
             // from the text input. indexOf() will return a 
             // value of 0 or more indicating the spot where the 
             // search term appears, or -1 if it doesn't appear.
             
-            // We'll add 1 so that the non-appearing words will be 
-            // falsey and all appearing words will be truthy. 
-            // We'll multiply these values by each other for each 
-            // word in the input box, since we want every term inputted
-            // to appear. 
+            // In that case, the lowest value for a word that does
+            // appear in itemInfo is 0, but we want all appearing words
+            // to return a truthy value, so we'll add 1. The 
+            // non-appearing words will go from -1 to 0, which is fine
+            // because that's still falsey.  
+
+            // We only want list items to appear if every word matches.
+            // That is, a zero for any search term should yield a zero 
+            // overall. So, using the loop below, we'll multiply the values
+            // for each search term to get an overall value that will be
+            // zero if any of the words doesn't appear.
             
             var value = 1;
-            for (i = 0; i < filterWords.length; i++) {
-                value *= (itemInfo.indexOf(filterWords[i]) + 1);
+            for (i = 0; i < self.filterWords.length; i++) {
+                value *= (itemInfo.indexOf(self.filterWords[i]) + 1);
             };
             return value;
         });
     });
 
+    window.addEventListener('input', function(event) {
+        console.log('input happened');
+        mapControl.renderMarkers(self.filterWords);
+    });
 };
 
 var mapControl = {
@@ -102,9 +114,18 @@ var mapControl = {
                 mapTypeControl: false
             });
 
+        // Create image for marker icon
+        this.markerImage = new google.maps.MarkerImage(
+          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|2B6D06|40|_|%E2%80%A2',
+            new google.maps.Size(21, 34),
+            new google.maps.Point(0, 0),
+            new google.maps.Point(10, 34),
+            new google.maps.Size(21,34)
+        );
+        
         // Create markers appearing on initialize
 
-        this.createMarkers();
+        this.renderMarkers([]);
 
     },
 
@@ -115,7 +136,7 @@ var mapControl = {
         // Then update which place is selected and re-render markers
         
     
-        mapControl.createMarkers();
+        mapControl.renderMarkers(viewModel.filterWords);
 
         // Then open the right infowindow
         
@@ -123,26 +144,55 @@ var mapControl = {
     
     },
         
-    createMarkers: function() {
-        var markerImage = new google.maps.MarkerImage(
-          'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|2B6D06|40|_|%E2%80%A2',
-            new google.maps.Size(21, 34),
-            new google.maps.Point(0, 0),
-            new google.maps.Point(10, 34),
-            new google.maps.Size(21,34));
+   renderMarkers: function(filterArray) {
+        
+       // First delete all existing markers.
+       
+       this.markers.forEach(function(element) {
+           element.setMap(null);
+       });
+       
+        // NOTE: Does the fact that this function is called
+        // from an event Listener set up within the viewModel break
+        // the rule of not having Knockout handle the Maps API?
+            
         for (i = 0; i < data.placeData.length; i++) {
-            var visibility = Boolean(data.placeData[i].title.indexOf(filter) + 1);
+            console.log(data.placeData.length);
+            console.log(data.placeData[i]);
             var marker = new google.maps.Marker({
                 position: data.placeData[i].position,
                 map: this.map,
                 title: data.placeData[i].title,
-                setVisible: visibility,
                 feature: data.placeData[i].feature,
-                icon: markerImage
+                icon: this.markerImage
             });
+                         
+            // Using the filterWords/filterArray value 
+            // passed to the function, render only the markers
+            // we want. 
+
+            // We'll use a similar technique to filter the markers
+            // as we used to filter the list in the viewModel.
+
+            var markerInfo = (marker.title + ' ' + marker.feature).toLowerCase();
+            console.log(markerInfo);
+            var value = 1;
+            for (j = 0; j < filterArray.length; j++) {
+                value *= (markerInfo.indexOf(filterArray[j]) + 1);
+            }
+            
+            console.log(marker.title + ' value is ' + value + ' or ' +  Boolean(value));
+            
+            
+            // Set the marker to be visible only if the value
+            // is true, then add it to the markers array.
+            marker.setVisible(Boolean(value));
             this.markers.push(marker);
             console.log(marker.title);
             var markerTitle = marker.title;
+            
+            // Add an event listener to each marker for
+            // being clicked.
             marker.addListener('click', function() {
                 // For some reason, "this.title" is locked
                 // to be whatever the very first click was on
@@ -154,7 +204,7 @@ var mapControl = {
                 mapControl.selectPlace(this);
                 mapControl.detailInfoWindow.open(map, this);  
             });
-        };
+        }
     },
     
     detailInfowindow: function(selectedMarker) {
